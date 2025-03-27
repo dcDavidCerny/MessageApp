f62339b38deea35d7f98d3f3a71ab49aa37de1a6
dcDavidCerny <dcdavidcerny@gmail.com>
2025-03-27 14:34:22 +0100

    Hash Router implementation; link to README; automatic buildiung + github pages

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { HashRouter } from "react-router";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +19,9 @@ export const queryClient = new QueryClient({
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
       <ReactQueryDevtools initialIsOpen position="right" />
     </QueryClientProvider>
   </StrictMode>
